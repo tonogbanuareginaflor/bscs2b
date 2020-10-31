d23886b6317e49b1d3924551e470052c2086c3a6
Natalie Jane Pacificar <55941769+Fynmn@users.noreply.github.com>
2020-10-31 19:28:46 +0800

    Create Activityiv.java

---
+++ Pacificar/Activity 5/Activityiv.java
@@ -0,0 +1,19 @@
+public class Activityiv {
+    public static void main(String[] args) {
+        int rows = 5;
+        int j = 0;
+
+        for(int i = 1; i <= rows; ++i, j = 0) {
+            for(int space = 1; space <= rows - i; ++space) {
+                System.out.print("  ");
+            }
+            while(j != 2 * i - 1) {
+                System.out.print("* ");
+                ++j;
+            }
+
+            System.out.println();
+        }
+    }
+
+}
